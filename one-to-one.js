4cf0b1e22601b3f6eb3fa25fb27ba16056673678
hnakao <34839422+hnakao@users.noreply.github.com>
2022-01-05 00:28:32 -0500

    Create one-to-one.js

---
+++ one-to-one.js
@@ -0,0 +1,11 @@
+(function (w, d, s, o, f, js, fjs) {
+            w["Onlive-Widget"] = o; w[o] = w[o] || function () {(w[o].q = w[o].q || []).push(arguments)};
+            js = d.createElement(s), fjs = d.getElementsByTagName(s)[0];
+            js.id = o; js.src = f; js.async = 1; fjs.parentNode.insertBefore(js, fjs);
+            } 
+        (window, document, "script", "onliveW", "https://qm349k-cdn.onlive.site/widget-staging/v1/staging-onlive-widget.js")
+        );
+        onliveW("init", {
+            targetElementId: "onlive-widget",
+            organizationId: "ec6423cb-5724-4418-a9ea-b3650fea2e34",
+    })
