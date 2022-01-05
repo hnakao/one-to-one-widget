b0136bb800d9256063e03233022e9300a0c3bbd5
hnakao <34839422+hnakao@users.noreply.github.com>
2022-01-05 11:36:28 -0500

    Update one-to-one.js

--- one-to-one.js
+++ one-to-one.js
@@ -1,45 +1,7 @@
 (function (w, d, s, o, f, js, fjs) {
-    var loadScript = function (url, callback) {
-        /* JavaScript that will load the jQuery library on Google's CDN.
-      We recommend this code: https://snipplr.com/view/18756/loadscript/.
-      Once the jQuery library is loaded, the callback function will be executed. */
-          callback();
-      };
-      
-      var $newdiv = $("<div id='onlive-widget'></div>");
-      
-      var myAppJavaScript = function ($) {
-        /* Your app's JavaScript here.
-      $ in this scope references the jQuery object we'll use.
-      Don't use 'jQuery', or 'jQuery191', here. Use the dollar sign
-      that was passed as argument.*/
-        $("body").append($newdiv);
-      };
-      
-      // For jQuery version 1.7
-      var target = [1, 7, 0];
-      
-      var current =
-        typeof jQuery === "undefined"
-          ? [0, 0, 0]
-          : $.fn.jquery.split(".").map(function (item) {
-              return parseInt(item);
-            });
-      
-      if (
-        current[0] < target[0] ||
-        (current[0] == target[0] && current[1] < target[1])
-      ) {
-        loadScript(
-          "//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js",
-          function () {
-            var jQuery1101 = jQuery.noConflict(true);
-            myAppJavaScript(jQuery1101);
-          }
-        );
-      } else {
-        myAppJavaScript(jQuery);
-      }
+    var elemDiv = document.createElement('div');
+    elemDiv.setAttribute("id", "onlive-widget");
+    document.body.appendChild(elemDiv);
     
   w["Onlive-Widget"] = o;
   w[o] =
