5d52ce7db6d9bbcb04da2966f97919b38d904701
Hiroshi Nakao <hnakao11@gmail.com>
2022-01-05 11:54:49 -0500

    Add widget feature

--- one-to-one.js
+++ one-to-one.js
@@ -1,3 +1,27 @@
-var elemDiv = document.createElement('div');
+(function (w, d, s, o, f, js, fjs) {
+    var elemDiv = document.createElement('div');
     elemDiv.setAttribute("id", "onlive-widget");
     document.body.appendChild(elemDiv);
+
+  w["Onlive-Widget"] = o;
+  w[o] =
+    w[o] ||
+    function () {
+      (w[o].q = w[o].q || []).push(arguments);
+    };
+  (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
+  js.id = o;
+  js.src = f;
+  js.async = 1;
+  fjs.parentNode.insertBefore(js, fjs);
+})(
+  window,
+  document,
+  "script",
+  "onliveW",
+  "https://cdn.onlive.site/widget/v1/onlive-widget.js"
+);
+onliveW("init", {
+  targetElementId: "onlive-widget",
+  organizationId: "ec6423cb-5724-4418-a9ea-b3650fea2e34",
+});
